abe5ba645036a3f3ba67eee2bed10f653d7213fa
Hoang Nguyen <hnguyen0428@yahoo.com>
2018-03-04 09:48:36 -0800

    local

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ from bing_search import *
 
 import os
 
-base_url = 'http://52.53.158.244/video/'
-base_url_original = 'http://52.53.158.244/video/original/'
-ROOT_FOLDER = '/var/www/html/caltech-ms-server/'
-VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/videos/'
-EDITED_VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/edited_videos/'
+# base_url = 'http://52.53.158.244/video/'
+# base_url_original = 'http://52.53.158.244/video/original/'
+# ROOT_FOLDER = '/var/www/html/caltech-ms-server/'
+# VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/videos/'
+# EDITED_VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/edited_videos/'
 
-# base_url = 'http://127.0.0.1:5000/video/'
-# ROOT_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/'
-# VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/videos/'
-# EDITED_VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/edited_videos/'
+base_url = 'http://127.0.0.1:5000/video/'
+base_url_original = 'http://127.0.0.1:5000/video/original/'
+ROOT_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/'
+VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/videos/'
+EDITED_VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/edited_videos/'
 
 app = Flask(__name__)
 CORS(app)
