c518703737c0fd7d6b87092c1536911132484657
Hoang Nguyen <hnguyen0428@yahoo.com>
2018-03-04 03:10:24 -0800

    server version

--- app.py
+++ app.py
@@ -7,17 +7,15 @@ from utility import *
 import video_parser
 from video_parser import HighlightMaker
 
-
 import os
-import sys
 
-# base_url = 'http://52.53.158.244/video/'
-# VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/videos/'
-# EDITED_VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/edited_videos/'
+base_url = 'http://52.53.158.244/video/'
+VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/videos/'
+EDITED_VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/edited_videos/'
 
-base_url = 'http://127.0.0.1:5000/video/'
-VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/videos/'
-EDITED_VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/edited_videos/'
+# base_url = 'http://127.0.0.1:5000/video/'
+# VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/videos/'
+# EDITED_VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/edited_videos/'
 
 app = Flask(__name__)
 CORS(app)
--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
-# base_url = 'http://18.144.27.216/video/'
-# VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/videos/'
-# EDITED_VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/edited_videos/'
+base_url = 'http://52.53.158.244/video/'
+VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/videos/'
+EDITED_VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/edited_videos/'
 
-base_url = 'http://127.0.0.1:5000/'
-VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/videos/'
-EDITED_VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/edited_videos/'
+# base_url = 'http://127.0.0.1:5000/'
+# VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/videos/'
+# EDITED_VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/edited_videos/'
