ad6bb7d59ab434ddd284c5fe09870e1e0c8c680e
Hoang Nguyen <hnguyen0428@yahoo.com>
2018-03-04 07:59:18 -0800

    changed to server path

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ from bing_search import *
 
 import os
 
-# base_url = 'http://52.53.158.244/video/'
-# VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/videos/'
-# EDITED_VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/edited_videos/'
+base_url = 'http://52.53.158.244/video/'
+VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/videos/'
+EDITED_VIDEOS_FOLDER = '/var/www/html/caltech-ms-server/edited_videos/'
 
-base_url = 'http://127.0.0.1:5000/video/'
-VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/videos/'
-EDITED_VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/edited_videos/'
+# base_url = 'http://127.0.0.1:5000/video/'
+# VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/videos/'
+# EDITED_VIDEOS_FOLDER = '/Users/hnguyen0428/Unsynced Files/CSRelated/caltech-ms-server/edited_videos/'
 
 app = Flask(__name__)
 CORS(app)
